7f5896dae9cbbe8e7b2236ecd6c65314f5110761
gaureenewatia <134475631+gaureenewatia@users.noreply.github.com>
2025-07-01 18:39:21 +0000

    conversion

--- 01_Basics/03_variableConversion.js
+++ 01_Basics/03_variableConversion.js
@@ -1,9 +1,18 @@
-let score = 33
-console.log(typeof(score));
+// let score = 33
+// console.log(typeof(score));
 
-let valueInNumber = Number(score) 
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber);
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3);
