2ce53777fe898742ba5f90aa67d2d8aa5a431456
gaureenewatia <134475631+gaureenewatia@users.noreply.github.com>
2025-07-01 18:57:50 +0000

    summary

---
+++ 01_Basics/05_dtsummary.js
@@ -0,0 +1,13 @@
+const id = Symbol('123')
+
+
+// Array, Object
+const heroes = ["shaktiman","naagraj","doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+const myFunction = function(){
+    console.log("Hello");
+    
+}
