f5a93159a1e92e6d72fe0f56d4e8874c59728428
gaureenewatia <134475631+gaureenewatia@users.noreply.github.com>
2025-07-01 17:49:05 +0000

    datatype

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountEmail = "himesh@gmail.com"
+var accountPassword = "1236"
+accountCity = "Jaipur"
+// accountId = 2
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict";// treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+// number => 2 to power 53
+//bigInt
+// string => ""
+// boolean => true/false
+// null=> standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);
